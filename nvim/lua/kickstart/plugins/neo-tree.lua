fbfa192cdd71c610eb58a8713ea378e6ed121009
Benjamen Pyle <benbpyle@gmail.com>
2024-11-18 21:18:00 -0800

    big change

---
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,34 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+return { -- So for api plugins like devicons, we can always set lazy=true
+{
+    'nvim-neo-tree/neo-tree.nvim',
+    version = '*',
+    dependencies = {'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim'},
+    cmd = 'Neotree',
+    keys = {{
+        '\\',
+        ':Neotree reveal<CR>',
+        desc = 'NeoTree reveal',
+        silent = true
+    }},
+    opts = {
+        filesystem = {
+            filtered_items = {
+                visible = true,
+                show_hidden_count = true,
+                hide_dotfiles = false,
+                hide_gitignored = false,
+                hide_by_name = {'.git', '.DS_Store', 'thumbs.db'},
+                never_show = {}
+            },
+            window = {
+                mappings = {
+                    ['\\'] = 'close_window'
+                }
+            }
+        }
+
+    }
+}}
